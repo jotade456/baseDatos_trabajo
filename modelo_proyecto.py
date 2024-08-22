efb51a4001a288e7bc01d1185a44532bce56fe0c
jotade456 <diazrojas7890@gmail.com>
2024-08-22 17:19:37 -0500

    Create modelo_proyecto.py
    modelo para verificar si el usuario existe en la base de datos

---
+++ modelo_proyecto.py
@@ -0,0 +1,21 @@
+import json
+import mysql.connector
+class conexion_BD:
+    def __init__(self):
+        self.conexion=mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="",
+            database="proyecto_base"
+        )
+        
+    def verificar_datos(self,nombre,contraseña,rol):
+        consulta = "SELECT COUNT(*) FROM usuario WHERE nombre = %s AND contraseña = %s AND rol = %s"
+        self.cursor.execute(consulta, (nombre, contraseña, rol))
+        resultado = self.cursor.fetchone()
+        return resultado[0] > 0 
+        
+        
+    def cerrar_conexion(self):
+        self.cursor.close()
+        self.conn.close()
